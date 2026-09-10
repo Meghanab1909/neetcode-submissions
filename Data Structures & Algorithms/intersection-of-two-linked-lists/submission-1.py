1ae3c1f9b5c812c5cd39825ed0f75d49e52069f2
Meghanab1909 <144342309+Meghanab1909@users.noreply.github.com>
2026-09-10 11:16:49 +0530

    Add: intersection-of-two-linked-lists - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-1.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        #SAME NODE NOT SAME VALUE
+        
+        listA = []
+        p = headA
+
+        while p:
+            listA.append(p)
+            p = p.next
+        
+        q = headB
+        found = 0
+
+        while q:
+            if q in listA:
+                return q
+                found = 1
+                break
+            q = q.next
+        
+        if found == 0:
+            return None
