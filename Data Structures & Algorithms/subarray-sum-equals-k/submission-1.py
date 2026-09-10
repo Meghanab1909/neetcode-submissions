de081d2742b42102c6b41198f697ec7b4c2f151d
Meghanab1909 <144342309+Meghanab1909@users.noreply.github.com>
2026-09-10 07:51:53 +0530

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        hashmap = {0:1}
+        count = 0
+        prefix_sum = 0
+
+        for i in nums:
+            prefix_sum += i
+
+            if (prefix_sum - k) in hashmap:
+                count = count + hashmap[prefix_sum - k]
+            
+            if prefix_sum not in hashmap:
+                hashmap[prefix_sum] = 1
+            else:
+                hashmap[prefix_sum] += 1
+        
+        return count
+        
